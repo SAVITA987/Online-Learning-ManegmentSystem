ae6a585042f372b8058ff174f4b0b3bfa489a767
savitatekale <110305120+SAVITA987@users.noreply.github.com>
2023-10-09 10:11:27 +0530

    OLMS-Frontend

---
+++ OLMS-Frontend/src/app/models/user.ts
@@ -0,0 +1,14 @@
+export class User 
+{
+
+    username : string = '';
+    userid : string = '';
+    email : string = '';
+    profession : string = '';
+    gender : string = '';
+    mobile : string = '';
+    address : string = '';
+    password : string = '';
+
+    constructor() {}
+}
